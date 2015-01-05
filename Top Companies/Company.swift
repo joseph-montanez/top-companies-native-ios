ddee3701e0048d76c2d215761a1a7560474a5566
Joseph Montanez <jm@shabb.com>
2015-01-05 09:17:40 -0800

    layout for listing, started company class

---
+++ Top Companies/Company.swift
@@ -0,0 +1,26 @@
+//
+//  Company.swift
+//  Top Companies
+//
+//  Created by Bernard Kohantob on 1/5/15.
+//  Copyright (c) 2015 minidev. All rights reserved.
+//
+
+import Foundation
+
+class TPCompany {
+    var id : Int = 0
+    var name : String = ""
+    var address : String = ""
+    var phone : String = ""
+    var city : String = ""
+    var state : String = ""
+    var website : String = ""
+    var zip : String = ""
+    var description : String = ""
+    var descriptionInternal : String = ""
+    var descriptionMeta : String = ""
+    var employeeNo : Int = 0
+    var revenue : Int = 0
+    var founded : Int = 0
+}
--- Top Companies/ListController.swift
+++ Top Companies/ListController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ListController : UIViewController {
+class ListController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var simpleTableIdentifier: String = "companyItemCell"
     var categoryId : String = ""
+    var results: [TPCompany] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,5 +25,40 @@ class ListController : UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // Table View Methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(tableView2: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = self.tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier) as UITableViewCell
+        var label: UILabel;
+        //        if cell == nil {
+        //            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
+        //        }
+        label = cell.contentView.viewWithTag(10) as UILabel;
+        //        cell.textLabel.text = keywordList[indexPath.row]
+        label.text = results[indexPath.row].name
+        
+        
+        //cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("Selected row! - \(indexPath.row)")
+        
+        let item = self.results[indexPath.row]
+        switch item.type {
+        case .Category:
+            let listController = getStoryBoard().instantiateViewControllerWithIdentifier("ListController") as ListController
+            listController.categoryId = item.id
+            self.presentViewController(listController, animated: true, completion: nil)
+        case .Company:
+            println("TODO: implement go to company")
+        }
     }
 }
