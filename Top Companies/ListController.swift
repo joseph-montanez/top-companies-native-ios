f13c7ced15bcd129b31163a8f92447a6d8e67113
Joseph Montanez <jm@shabb.com>
2015-01-05 01:53:04 -0800

    switch image to button added back state

--- Top Companies/ListController.swift
+++ Top Companies/ListController.swift
@@ -16,6 +16,10 @@ class ListController : UIViewController {
         println("Loaded! with category \(categoryId)")
     }
     
+    @IBAction func goBack(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
