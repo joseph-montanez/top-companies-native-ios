b0fc6fe668640c272c6047f43d84e651b45b141e
Joseph Montanez <jm@shabb.com>
2015-04-05 22:34:23 -0700

    finished layout, iphone 6/+ fixes

--- Top Companies/CompanyDetailViewController.swift
+++ Top Companies/CompanyDetailViewController.swift
@@ -42,7 +42,37 @@ class CompanyDetailViewController: UIViewController {
     @IBOutlet weak var overviewTextBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var overviewTextHeightConstraint: NSLayoutConstraint!
     
-    @IBOutlet weak var noOfEmployees: UILabel!
+    // Description
+    @IBOutlet weak var descriptionText: UILabel!
+    @IBOutlet weak var descriptionTextHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var descriptionTextTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var descriptionTextBottomConstraint: NSLayoutConstraint!
+    
+    // Business
+    @IBOutlet weak var businessText: UILabel!
+    @IBOutlet weak var businessTextTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var businessTextHeightConstraint: NSLayoutConstraint!
+    
+    // Year
+    @IBOutlet weak var yearEstText: UILabel!
+    @IBOutlet weak var yearEst: UILabel!
+    @IBOutlet weak var yearEstTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var yearEstTextHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var yearEstHeightConstraint: NSLayoutConstraint!
+    
+    // Annual Revenue
+    @IBOutlet weak var annualRevText: UILabel!
+    @IBOutlet weak var annualRev: UILabel!
+    @IBOutlet weak var annualRevTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var annualRevTextHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var annualRevHeightConstraint: NSLayoutConstraint!
+    
+    // Number of Employees
+    @IBOutlet weak var employeeNoText: UILabel!
+    @IBOutlet weak var employeeNo: UILabel!
+    @IBOutlet weak var employeeNoTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var employeeNoTextHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var employeeNoHeightConstraint: NSLayoutConstraint!
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -62,16 +92,23 @@ class CompanyDetailViewController: UIViewController {
             println("company has details")
             //-- TODO Randomize Banner
                 
-            self.companyName.text = company.name
-            self.address.text = company.address
-            self.cityStateZip.text = company.city
-            self.website.text = company.website
-            self.phone.text = company.phone
+            companyName.text = company.name
+            address.text = company.address
+            cityStateZip.text = company.city
+            website.text = company.website
+            phone.text = company.phone
+            overviewText.text = company.description
+            descriptionText.text = company.descriptionMeta
+            yearEst.text = String(company.founded)
             
-            self.overviewText.text = company.description
+            var formatter = NSNumberFormatter()
+            formatter.numberStyle = .CurrencyStyle
+            annualRev.text = formatter.stringFromNumber(company.revenue)
+            
+            employeeNo.text = String(company.employeeNo)
+            
             
             if countElements(company.description) == 0 {
-                //            overviewTitleCell.hidden = true
                 overviewTitleText.text = ""
                 overviewTitleTextTopConstraint.constant = 0
                 overviewTitleTextBetweenConstraint.constant = 0
@@ -79,23 +116,79 @@ class CompanyDetailViewController: UIViewController {
                 overviewTitleTextHeightConstraint.priority = 750
                 
                 
-                //            overviewCell.hidden = true
                 overviewTextBottomConstraint.constant = 0
                 overviewTextHeightConstraint.constant = 0
                 overviewTextHeightConstraint.priority = 750
             } else {
-//                overviewTitleCell.hidden = false
                 overviewTitleText.text = "Overview"
                 overviewTitleTextTopConstraint.constant = 15
                 overviewTitleTextBetweenConstraint.constant = 4
                 overviewTitleTextHeightConstraint.priority = 250
                 
-//                overviewCell.hidden = false
                 overviewTextBottomConstraint.constant = 20
                 overviewTextHeightConstraint.priority = 250
             }
             
-            //scrollContrainerView.invalidateIntrinsicContentSize()
+            if countElements(company.descriptionMeta) == 0 {
+                descriptionTextTopConstraint.constant = 0
+                descriptionTextBottomConstraint.constant = 0
+                descriptionTextHeightConstraint.constant = 0
+                descriptionTextHeightConstraint.priority = 750
+            } else {
+                descriptionTextTopConstraint.constant = 15
+                descriptionTextBottomConstraint.constant = 20
+                descriptionTextHeightConstraint.priority = 250
+            }
+            
+            if company.employeeNo == 0 && company.revenue == 0 && company.founded == 0 {
+                businessText.text = ""
+                businessTextTopConstraint.constant = 0
+                businessTextHeightConstraint.constant = 0
+                businessTextHeightConstraint.priority = 750
+                
+            } else {
+                businessText.text = "Business Information"
+                businessTextTopConstraint.constant = 15
+                businessTextHeightConstraint.priority = 250
+            }
+            
+            if company.founded == 0 {
+                yearEst.text = ""
+                yearEstText.text = ""
+                yearEstTextHeightConstraint.constant = 0
+                yearEstTextHeightConstraint.priority = 750
+                yearEstTopConstraint.constant = 0
+            } else {
+                yearEstText.text = "Year Established:"
+                yearEstTextHeightConstraint.priority = 250
+                yearEstTopConstraint.constant = 8
+            }
+            
+            if company.revenue == 0 {
+                annualRev.text = ""
+                annualRevText.text = ""
+                annualRevTextHeightConstraint.constant = 0
+                annualRevTextHeightConstraint.priority = 750
+                annualRevTopConstraint.constant = 0
+            } else {
+                annualRevText.text = "Annual Revenue:"
+                annualRevTextHeightConstraint.priority = 250
+                annualRevTopConstraint.constant = 8
+            }
+            
+            if company.employeeNo == 0 {
+                employeeNo.text = ""
+                employeeNoText.text = ""
+                employeeNoTextHeightConstraint.constant = 0
+                employeeNoTextHeightConstraint.priority = 750
+                employeeNoTopConstraint.constant = 0
+            } else {
+                employeeNoText.text = "No. Employees:"
+                employeeNoTextHeightConstraint.priority = 250
+                employeeNoTopConstraint.constant = 8
+            }
+            
+            
             resizeScroll()
         } else {
             println("company is nil")
@@ -107,17 +200,11 @@ class CompanyDetailViewController: UIViewController {
     func resizeScroll() {
         view.layoutIfNeeded()
         
-        //-- Brute Force It ~ 8x slower to calculate then getting the last time
         var contentRect = CGRectZero
         for view in scrollContrainerView.subviews {
             contentRect = CGRectUnion(contentRect, view.frame)
         }
         scrollContainerViewHeight.constant = contentRect.height + 20
-        
-        //-- Get last item ~ Fastest Method?
-//        let localPoint: CGPoint = noOfEmployees.bounds.origin
-//        let basePoint: CGPoint = noOfEmployees.convertPoint(localPoint, fromCoordinateSpace: scrollView)
-//        scrollContainerViewHeight.constant = basePoint.y + noOfEmployees.bounds.height + 20
     }
     
     @IBAction func goBack(sender: UIButton) {
